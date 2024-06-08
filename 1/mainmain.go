7ff1c34ed3776786a4003c4f061f3361ed676225
AGamiSu <serj12343211@gmail.com>
2024-06-08 15:07:02 +0300

    а

---
+++ 1/mainmain.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var message string 
+	var number float32
+	var b bool
+
+	message = "hui"
+	number = 12.2
+
+	fmt.Println(message)
+	fmt.Println(number)
+	fmt.Println(b)
+
+}
