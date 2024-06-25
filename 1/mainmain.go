1904493d49fee0c482b3f0ef0727a5b49337f030
AGamiSu <serj12343211@gmail.com>
2024-06-25 19:27:15 +0300

    kghf

--- 1/mainmain.go
+++ 1/mainmain.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	a, b, c := 1, 2, 3
 	fmt.Println(a, b, c)
+	print()
 }
-func print(){
-	
+func print() {
+	a, b, c := 4, 5, 6
+	fmt.Println(a, b, c)
 }
