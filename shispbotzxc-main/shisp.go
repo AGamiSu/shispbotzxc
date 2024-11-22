972ac66af22dfc7e285909e2fc4470a655a21d70
AGamiSu <serj12343211@gmail.com>
2024-11-22 06:44:35 +0300

    stage1

--- shispbotzxc-main/shisp.go
+++ shispbotzxc-main/shisp.go
@@ -1,57 +1,35 @@
 package main
 
 import (
-	"log"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    "log"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+const botToken = "7625350088:AAGY2qai8kYZ9cwbowBODmOtFlwjhoO8ubM"
+
 func main() {
-	// Инициализация бота
-	bot, err := tgbotapi.NewBotAPI("7625350088:AAGY2qai8kYZ9cwbowBODmOtFlwjhoO8ubM")
-	if err != nil {
-		log.Panic(err)
-	}
+    bot, err := tgbotapi.NewBotAPI(botToken)
+    if err != nil {
+        log.Panic(err)
+    }
 
-	bot.Debug = true
-	log.Printf("Бот запущен: %s", bot.Self.UserName)
+    bot.Debug = true
+    log.Printf("Бот запущен: %s", bot.Self.UserName)
 
-	// Получение обновлений от Telegram
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates := bot.GetUpdatesChan(u)
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
 
-	// Обработка обновлений
-	for update := range updates {
-		if update.Message != nil { // Проверяем, что это сообщение
-			if update.Message.IsCommand() { // Если это команда
-				switch update.Message.Command() {
-				case "start":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет! Чтобы получить доступ в приватный чат, пожалуйста, оплатите доступ.")
-					bot.Send(msg)
-				case "pay":
-					// Условная проверка "оплаты"
-					if update.Message.Text == "оплачено" {
-						chatID := int64(-123456789) // Заменить на реальный ID приватного чата
+    updates := bot.GetUpdatesChan(u)
 
-						// Получение ссылки на чат
-						inviteLink, err := bot.ExportChatInviteLink(tgbotapi.ChatInviteLinkConfig{
-							ChatID: chatID,
-						})
-						if err != nil {
-							log.Println("Ошибка создания ссылки:", err)
-							msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ошибка доступа в чат. Попробуйте позже.")
-							bot.Send(msg)
-						} else {
-							msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Оплата подтверждена! Вот ваша ссылка в чат: "+inviteLink.InviteLink)
-							bot.Send(msg)
-						}
-					} else {
-						msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Оплата не подтверждена. Пожалуйста, проверьте данные.")
-						bot.Send(msg)
-					}
-				}
-			}
-		}
-	}
+    for update := range updates {
+        if update.Message != nil { // Проверяем, что это сообщение
+            if update.Message.IsCommand() { // Если это команда
+                switch update.Message.Command() {
+                case "start":
+                    msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет! Я SHISPbot, чтобы получить доступ в приватный чат, пожалуйста, оплатите доступ.")
+                    bot.Send(msg)
+                }
+            }
+        }
+    }
 }
